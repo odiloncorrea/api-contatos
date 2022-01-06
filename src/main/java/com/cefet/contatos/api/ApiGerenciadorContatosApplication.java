9647c522553591c9994484f3ed1558ee18ea4ee8
Odilon Corrêa <odilon.correa@gmail.com>
2022-01-06 00:00:14 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/main/java/com/cefet/contatos/api/ApiGerenciadorContatosApplication.java
@@ -0,0 +1,13 @@
+package com.cefet.contatos.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciadorContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGerenciadorContatosApplication.class, args);
+	}
+
+}
